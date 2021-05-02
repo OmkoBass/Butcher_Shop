82891ffae03842f0773f95e8812f6725c3bdea85
OmkoBass <omkob@hotmail.com>
2021-05-02 23:35:59 +0200

    Added dtos

---
+++ Backend/Butcher_Shop/Dtos/ButcherStoreDto.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Butcher_Shop.Dtos
+{
+    public class ButcherStoreDto
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Address { get; set; }
+        public int Area { get; set; }
+    }
+}
