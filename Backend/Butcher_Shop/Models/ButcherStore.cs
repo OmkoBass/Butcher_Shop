b780a83e8af463ccf165d3cc62d6b4420fe64d3d
OmkoBass <omkob@hotmail.com>
2021-05-02 23:36:17 +0200

    Added a name field for the butcher store

--- Backend/Butcher_Shop/Models/ButcherStore.cs
+++ Backend/Butcher_Shop/Models/ButcherStore.cs
@@ -15,6 +15,11 @@ namespace Butcher_Shop.Models
 
         [Required]
         [MinLength(3)]
+        [MaxLength(32)]
+        public string Name { get; set; }
+
+        [Required]
+        [MinLength(3)]
         [MaxLength(64)]
         public string Address { get; set; }
 
