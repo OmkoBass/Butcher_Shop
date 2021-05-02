a5a864e10c7c8e9aa0b6453046a702471350e78e
OmkoBass <omkob@hotmail.com>
2021-05-02 23:36:41 +0200

    Added profiles for automapper

---
+++ Backend/Butcher_Shop/Profiles/ButcherProfile.cs
@@ -0,0 +1,18 @@
+﻿using AutoMapper;
+using Butcher_Shop.Dtos;
+using Butcher_Shop.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Butcher_Shop.Profiles
+{
+    public class ButcherProfile : Profile
+    {
+        public ButcherProfile()
+        {
+            CreateMap<Butcher, ButcherDto>();
+        }
+    }
+}
