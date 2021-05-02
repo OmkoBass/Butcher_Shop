bb41c600490a0196b52873b06a60ad261edaa683
OmkoBass <omkob@hotmail.com>
2021-05-02 14:13:36 +0200

    Models

---
+++ Backend/Butcher_Shop/Models/Butcher.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Butcher_Shop.Models
+{
+    public class Butcher
+    {
+        public Butcher() => this.ButcherStores = new HashSet<ButcherStore>();
+
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int Id { get; set; }
+
+        [Required]
+        public string Name { get; set; }
+
+        [Required]
+        [MinLength(3)]
+        [MaxLength(16)]
+        public string Username { get; set; }
+
+        [Required]
+        [MinLength(8)]
+        [MaxLength(16)]
+        public string Password { get; set; }
+
+        public ICollection<ButcherStore> ButcherStores { get; set; }
+    }
+}
---
+++ Backend/Butcher_Shop/Models/ButcherStore.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Butcher_Shop.Models
+{
+    public class ButcherStore
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int Id { get; set; }
+
+        [Required]
+        [MinLength(3)]
+        [MaxLength(64)]
+        public string Address { get; set; }
+
+        [Range(0, Int32.MaxValue)]
+        public int Area { get; set; }
+
+        public int ButcherId { get; set; }
+        public virtual Butcher Butcher { get; set; }
+    }
+}
