3b09324fa675ba9d9b2a481b2daddcba2c4e9fb3
OmkoBass <omkob@hotmail.com>
2021-05-02 23:36:27 +0200

    Migrations

---
+++ Backend/Butcher_Shop/Migrations/20210502133251_ButcherStoreName.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Butcher_Shop.Migrations
+{
+    public partial class ButcherStoreName : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "Name",
+                table: "ButcherStores",
+                type: "nvarchar(32)",
+                maxLength: 32,
+                nullable: false,
+                defaultValue: "");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Name",
+                table: "ButcherStores");
+        }
+    }
+}
