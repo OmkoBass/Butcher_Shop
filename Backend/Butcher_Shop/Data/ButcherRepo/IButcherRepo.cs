5cd578fe06cc041b21e206ad8974edc63cf3f315
OmkoBass <omkob@hotmail.com>
2021-05-02 14:13:14 +0200

    Added Butcher Repo

---
+++ Backend/Butcher_Shop/Data/ButcherRepo/IButcherRepo.cs
@@ -0,0 +1,17 @@
+﻿using Butcher_Shop.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Butcher_Shop.Data.ButcherRepo
+{
+    public interface IButcherRepo
+    {
+        public Task<List<Butcher>> GetAllButchers();
+        public Task<Butcher> GetButcher(int Id);
+        public Task<Butcher> AddButcher(Butcher Butcher);
+        public Task<Butcher> UpdateButcher(Butcher Butcher);
+        public Task<bool> DeleteButcher(int Id);
+    }
+}
---
+++ Backend/Butcher_Shop/Data/ButcherRepo/MockButcherRepo.cs
@@ -0,0 +1,74 @@
+﻿using Butcher_Shop.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Butcher_Shop.Data.ButcherRepo
+{
+    public class MockButcherRepo : IButcherRepo
+    {
+        private readonly DatabaseContext _context;
+        public MockButcherRepo(DatabaseContext context) => _context = context;
+
+        public async Task<Butcher> AddButcher(Butcher Butcher)
+        {
+            var AddedButcher = await _context.AddAsync(Butcher);
+
+            if (AddedButcher != null)
+            {
+                await _context.SaveChangesAsync();
+                return AddedButcher.Entity;
+            }
+            return null;
+        }
+
+        public async Task<bool> DeleteButcher(int Id)
+        {
+            var FoundButcher = await _context.Butchers.FindAsync(Id);
+
+            if(FoundButcher != null)
+            {
+                _context.Butchers.Remove(FoundButcher);
+                await _context.SaveChangesAsync();
+
+                return true;
+            }
+
+            return false;
+        }
+
+        public async Task<List<Butcher>> GetAllButchers()
+        {
+            return await _context.Butchers.ToListAsync();
+        }
+
+        public async Task<Butcher> GetButcher(int Id)
+        {
+            var FoundButcher = await _context.Butchers.FindAsync(Id);
+
+            if (FoundButcher != null)
+            {
+                return await _context.Butchers.FindAsync(Id);
+            }
+
+            return null;
+        }
+
+        public async Task<Butcher> UpdateButcher(Butcher Butcher)
+        {
+            var FoundButcher = await _context.Butchers.FindAsync(Butcher.Id);
+
+            if (FoundButcher != null)
+            {
+                var UpdatedButcher = _context.Butchers.Update(FoundButcher);
+                await _context.SaveChangesAsync();
+
+                return UpdatedButcher.Entity;
+            }
+
+            return null;
+        }
+    }
+}
