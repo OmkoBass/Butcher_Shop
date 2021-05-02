a64fad610407ab476c07549d9b328512ec604bff
OmkoBass <omkob@hotmail.com>
2021-05-02 14:13:26 +0200

    Migrations

---
+++ Backend/Butcher_Shop/Migrations/20210501221530_Initial.cs
@@ -0,0 +1,60 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Butcher_Shop.Migrations
+{
+    public partial class Initial : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Butchers",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Name = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Username = table.Column<string>(type: "nvarchar(16)", maxLength: 16, nullable: false),
+                    Password = table.Column<string>(type: "nvarchar(16)", maxLength: 16, nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Butchers", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "ButcherStores",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Address = table.Column<string>(type: "nvarchar(64)", maxLength: 64, nullable: false),
+                    Area = table.Column<int>(type: "int", nullable: false),
+                    ButcherId = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_ButcherStores", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_ButcherStores_Butchers_ButcherId",
+                        column: x => x.ButcherId,
+                        principalTable: "Butchers",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_ButcherStores_ButcherId",
+                table: "ButcherStores",
+                column: "ButcherId");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "ButcherStores");
+
+            migrationBuilder.DropTable(
+                name: "Butchers");
+        }
+    }
+}
