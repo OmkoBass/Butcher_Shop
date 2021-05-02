cee134bd446dec43ca4b441bdc110d9c1584e8a8
OmkoBass <omkob@hotmail.com>
2021-05-02 14:12:58 +0200

    Added database context

---
+++ Backend/Butcher_Shop/Data/DatabaseContext.cs
@@ -0,0 +1,29 @@
+﻿using Butcher_Shop.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Butcher_Shop.Data
+{
+    public class DatabaseContext : DbContext
+    {
+        public DatabaseContext(DbContextOptions<DatabaseContext> options) : base(options)
+        {
+
+        }
+
+        public DbSet<Butcher> Butchers { get; set; }
+        public DbSet<ButcherStore> ButcherStores { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<ButcherStore>()
+                .HasOne<Butcher>(ButcherStore => ButcherStore.Butcher)
+                .WithMany(Butcher => Butcher.ButcherStores)
+                .HasForeignKey(ButcherStore => ButcherStore.ButcherId)
+                .OnDelete(DeleteBehavior.Cascade);
+        }
+    }
+}
