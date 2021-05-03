39fde75805322530f7d7044b1c715860585034ce
OmkoBass <omkob@hotmail.com>
2021-05-03 15:38:29 +0200

    Added StorageArticle

---
+++ Backend/Butcher_Shop/Models/StorageArticle.cs
@@ -0,0 +1,19 @@
+﻿using Butcher_Shop.Dtos;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Butcher_Shop.Models
+{
+    public class StorageArticle
+    {
+        public int StorageId { get; set; }
+        public virtual Storage Storage { get; set; }
+
+        public int ArticleId { get; set; }
+        public virtual Article Article { get; set; }
+
+        public int Quantity { get; set; }
+    }
+}
