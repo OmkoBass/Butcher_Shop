5893b66d0de567c8a55f8ee45abd5ebe669d49d4
OmkoBass <omkob@hotmail.com>
2021-05-03 15:38:09 +0200

    Added storage

---
+++ Backend/Butcher_Shop/Models/Storage.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Butcher_Shop.Models
+{
+    public class Storage
+    {
+        public Storage() => this.StorageArticles = new HashSet<StorageArticle>();
+
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int Id { get; set; }
+
+        [Required]
+        [MinLength(3)]
+        [MaxLength(64)]
+        public string Address { get; set; }
+
+        [Range(0, Int32.MaxValue)]
+        public int Area { get; set; }
+
+        [Required]
+        public int ButcherStoreId { get; set; }
+        public virtual ButcherStore ButcherStore { get; set; }
+        public virtual ICollection<StorageArticle> StorageArticles { get; set; }
+    }
+}
