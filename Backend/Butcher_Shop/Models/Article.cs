f2f1d32f8c463803afdd24dc3f210f44b79e95d7
OmkoBass <omkob@hotmail.com>
2021-05-03 15:37:52 +0200

    Added article

---
+++ Backend/Butcher_Shop/Models/Article.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Butcher_Shop.Models
+{
+    public class Article
+    {
+        public Article() => this.StorageArticles = new HashSet<StorageArticle>();
+
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int Id { get; set; }
+
+        [Required]
+        [MinLength(3)]
+        [MaxLength(64)]
+        public string Naziv { get; set; }
+
+        public virtual ICollection<StorageArticle> StorageArticles { get; set; }
+    }
+}
