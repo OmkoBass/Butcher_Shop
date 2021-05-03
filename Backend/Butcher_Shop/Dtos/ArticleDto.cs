5f40c42132fbd6147d3106dabcb94837eda2dbe5
OmkoBass <omkob@hotmail.com>
2021-05-03 15:38:39 +0200

    Dtos for models

---
+++ Backend/Butcher_Shop/Dtos/ArticleDto.cs
@@ -0,0 +1,15 @@
+﻿using Butcher_Shop.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Butcher_Shop.Dtos
+{
+    public class ArticleDto
+    {
+        public string Naziv { get; set; }
+
+        public virtual ICollection<StorageArticle> StorageArticles { get; set; }
+    }
+}
---
+++ Backend/Butcher_Shop/Dtos/StorageDto.cs
@@ -0,0 +1,19 @@
+﻿using Butcher_Shop.Migrations;
+using Butcher_Shop.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Butcher_Shop.Dtos
+{
+    public class StorageDto
+    {
+        public int Id { get; set; }
+        public string Address { get; set; }
+        public int Area { get; set; }
+        public int ButcherStoreId { get; set; }
+        public virtual ButcherStoreDto ButcherStore { get; set; }
+        public virtual ICollection<Models.StorageArticle> StorageArticles { get; set; }
+    }
+}
