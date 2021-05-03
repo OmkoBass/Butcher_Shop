dc1dc4250d669aa16b40dcf2f1a7d90481d8029d
OmkoBass <omkob@hotmail.com>
2021-05-03 15:39:07 +0200

    Storage repo

---
+++ Backend/Butcher_Shop/Data/StorageRepo/IStorageRepo.cs
@@ -0,0 +1,17 @@
+﻿using Butcher_Shop.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Butcher_Shop.Data.StorageRepo
+{
+    public interface IStorageRepo
+    {
+        public Task<List<Storage>> GetAllStorages();
+        public Task<Storage> GetStorage(int Id);
+        public Task<Storage> AddStorage(Storage Storage);
+        public Task<Storage> UpdateStorage(int Id, Storage Storage);
+        public Task<bool> DeleteStorage(int Id);
+    }
+}
---
+++ Backend/Butcher_Shop/Data/StorageRepo/MockStorageRepo.cs
@@ -0,0 +1,75 @@
+﻿using Butcher_Shop.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Butcher_Shop.Data.StorageRepo
+{
+    public class MockStorageRepo : IStorageRepo
+    {
+        private readonly DatabaseContext _context;
+
+        public MockStorageRepo(DatabaseContext context) => _context = context;
+
+        public async Task<Storage> AddStorage(Storage Storage)
+        {
+            var AddedStorage = await _context.Storages.AddAsync(Storage);
+
+            if (AddedStorage != null)
+            {
+                await _context.SaveChangesAsync();
+                return AddedStorage.Entity;
+            }
+            return null;
+        }
+
+        public async Task<bool> DeleteStorage(int Id)
+        {
+            var FoundStorage = await _context.Storages.FindAsync(Id);
+
+            if (FoundStorage != null)
+            {
+                _context.Storages.Remove(FoundStorage);
+                await _context.SaveChangesAsync();
+
+                return true;
+            }
+
+            return false;
+        }
+
+        public async Task<List<Storage>> GetAllStorages()
+        {
+            return await _context.Storages.Include(s => s.StorageArticles).ToListAsync();
+        }
+
+        public async Task<Storage> GetStorage(int Id)
+        {
+            var FoundStorage = await _context.Storages.FindAsync(Id);
+
+            if (FoundStorage != null)
+            {
+                return await _context.Storages.Include(s => s.StorageArticles).FirstOrDefaultAsync(i => i.Id == Id);
+            }
+
+            return null;
+        }
+
+        public async Task<Storage> UpdateStorage(int Id, Storage Storage)
+        {
+            var FoundStorage = await _context.Storages.FindAsync(Id);
+
+            if (FoundStorage != null)
+            {
+                var UpdatedStorage = _context.Storages.Update(FoundStorage);
+                await _context.SaveChangesAsync();
+
+                return UpdatedStorage.Entity;
+            }
+
+            return null;
+        }
+    }
+}
