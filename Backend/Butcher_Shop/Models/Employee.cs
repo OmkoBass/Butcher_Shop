1f9ed3a462735fa39d960c014819c67d830e761c
OmkoBass <omkob@hotmail.com>
2021-05-04 16:29:21 +0200

    Added Employee Model

---
+++ Backend/Butcher_Shop/Models/Employee.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Butcher_Shop.Models
+{
+    public class Employee
+    {
+        public enum JobType
+        {
+            Cashier,
+            Administration
+        }
+
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int Id { get; set; }
+
+        [Required]
+        [MinLength(3)]
+        [MaxLength(16)]
+        public string Name { get; set; }
+
+        [Required]
+        [MinLength(3)]
+        [MaxLength(32)]
+        public string Lastname { get; set; }
+
+        [Required]
+        public bool Sex { get; set; }
+
+        [Required]
+        [MinLength(3)]
+        [MaxLength(64)]
+        public string Address { get; set; }
+
+        public JobType Job {get; set;}
+
+        [Required]
+        public int ButcherStoreId { get; set; }
+        public virtual ButcherStore ButcherStore { get; set; }
+    }
+}
