8867f858375af83c9f66054bf0edbef19da3affd
OmkoBass <omkob@hotmail.com>
2021-05-04 18:16:11 +0200

    Added a authorization model

---
+++ Backend/Butcher_Shop/Models/AuthButcher.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Butcher_Shop.Models
+{
+    public class AuthButcher
+    {
+        [Required]
+        [MinLength(3)]
+        [MaxLength(16)]
+        public string Username { get; set; }
+
+        [Required]
+        [MinLength(8)]
+        [MaxLength(64)]
+        public string Password { get; set; }
+    }
+}
