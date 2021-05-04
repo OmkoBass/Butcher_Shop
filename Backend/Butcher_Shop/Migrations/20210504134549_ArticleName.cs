b6824b40719ba95d6bdfbd1f2abab5c5da6a4bce
OmkoBass <omkob@hotmail.com>
2021-05-04 16:27:22 +0200

    Migrations

---
+++ Backend/Butcher_Shop/Migrations/20210504134549_ArticleName.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Butcher_Shop.Migrations
+{
+    public partial class ArticleName : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "Naziv",
+                table: "Articles",
+                newName: "Name");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Butchers_Username",
+                table: "Butchers",
+                column: "Username",
+                unique: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropIndex(
+                name: "IX_Butchers_Username",
+                table: "Butchers");
+
+            migrationBuilder.RenameColumn(
+                name: "Name",
+                table: "Articles",
+                newName: "Naziv");
+        }
+    }
+}
