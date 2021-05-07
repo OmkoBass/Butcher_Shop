6ec6a28963bdb131ffa18bf6d3c11891a5e1b19e
OmkoBass <omkob@hotmail.com>
2021-05-07 18:32:09 +0200

    Added migrations

---
+++ Backend/Butcher_Shop/Migrations/20210507163147_ArticleUniqueName.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Butcher_Shop.Migrations
+{
+    public partial class ArticleUniqueName : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateIndex(
+                name: "IX_Articles_Name",
+                table: "Articles",
+                column: "Name",
+                unique: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropIndex(
+                name: "IX_Articles_Name",
+                table: "Articles");
+        }
+    }
+}
