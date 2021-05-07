336c4a7c31349de51bae65aad7ace88b1874f8cf
OmkoBass <omkob@hotmail.com>
2021-05-07 17:06:41 +0200

    Added mapper profile for employee

---
+++ Backend/Butcher_Shop/Profiles/EmployeeProfile.cs
@@ -0,0 +1,18 @@
+﻿using AutoMapper;
+using Butcher_Shop.Dtos;
+using Butcher_Shop.Migrations;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Butcher_Shop.Profiles
+{
+    public class EmployeeProfile : Profile
+    {
+        public EmployeeProfile()
+        {
+            CreateMap<Butcher_Shop.Models.Employee, EmployeeDto>().ReverseMap();
+        }
+    }
+}
