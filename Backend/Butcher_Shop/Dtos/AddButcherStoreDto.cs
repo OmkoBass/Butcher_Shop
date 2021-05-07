a15fa953b310b13eee45460987019bb5b52c8c82
OmkoBass <omkob@hotmail.com>
2021-05-07 17:07:14 +0200

    Added dtos for butcher model

---
+++ Backend/Butcher_Shop/Dtos/AddButcherStoreDto.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+using Butcher_Shop.Models;
+
+namespace Butcher_Shop.Dtos
+{
+    public class AddButcherStoreDto
+    {
+        [Required]
+        [MinLength(3)]
+        [MaxLength(32)]
+        public string Name { get; set; }
+
+        [Required]
+        [MinLength(3)]
+        [MaxLength(64)]
+        public string Address { get; set; }
+
+        [Range(0, Int32.MaxValue)]
+        public int Area { get; set; }
+
+        [Required]
+        public int ButcherId { get; set; }
+    }
+}
