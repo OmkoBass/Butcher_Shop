69918bf49990c2d90d7ef0e45c3e5e01e90e4427
OmkoBass <omkob@hotmail.com>
2021-05-07 17:04:58 +0200

    Added employee Dto

---
+++ Backend/Butcher_Shop/Dtos/EmployeeDto.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Butcher_Shop.Dtos
+{
+    public class EmployeeDto
+    {
+        public enum JobType
+        {
+            Cashier = 0,
+            Administration = 1
+        }
+
+        [Required]
+        [MinLength(3)]
+        [MaxLength(16)]
+        public string Name { get; set; }
+
+        [Required]
+        [MinLength(3)]
+        [MaxLength(32)]
+        public string Lastname { get; set; }
+
+        [Required]
+        public bool Sex { get; set; }
+
+        [Required]
+        [MinLength(3)]
+        [MaxLength(64)]
+        public string Address { get; set; }
+
+        public JobType Job { get; set; }
+
+        [Required]
+        public int ButcherStoreId { get; set; }
+    }
+}
