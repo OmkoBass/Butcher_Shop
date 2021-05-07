2cff79caad72ff72a891e07486172df54de7eaeb
OmkoBass <omkob@hotmail.com>
2021-05-07 23:03:19 +0200

    Added auth guard

---
+++ Frontend/butcher-shop/src/app/guards/auth.guard.ts
@@ -0,0 +1,21 @@
+import { Injectable } from '@angular/core';
+import { ActivatedRoute, ActivatedRouteSnapshot, CanActivate, Router, RouterStateSnapshot, UrlTree } from '@angular/router';
+import { Observable } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AuthGuard implements CanActivate {
+  constructor(private router: Router, private route: ActivatedRoute) {}
+
+  canActivate(
+    route: ActivatedRouteSnapshot,
+    state: RouterStateSnapshot): Observable<boolean | UrlTree> | Promise<boolean | UrlTree> | boolean | UrlTree {
+      if(localStorage.getItem('beefyToken') !== null) {
+        return true;
+      }
+      this.router.navigate(['login']);
+      return false;
+  }
+  
+}
