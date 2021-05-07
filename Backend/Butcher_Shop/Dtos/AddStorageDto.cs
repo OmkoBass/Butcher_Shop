312e454b2157ee7911ff8b3c0fcc738b8bbd9ce9
OmkoBass <omkob@hotmail.com>
2021-05-07 17:59:17 +0200

    Added new storage dto

---
+++ Backend/Butcher_Shop/Dtos/AddStorageDto.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Butcher_Shop.Dtos.Storage
+{
+    public class AddStorageDto
+    {
+        [Required]
+        [MinLength(3)]
+        [MaxLength(64)]
+        public string Address { get; set; }
+
+        [Required]
+        [Range(0, Int32.MaxValue)]
+        public int Area { get; set; }
+
+        [Required]
+        public int ButcherStoreId { get; set; }
+    }
+}
