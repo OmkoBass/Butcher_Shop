5d48b67c0f08567e8ef02990e75ba94be179c811
OmkoBass <omkob@hotmail.com>
2021-05-07 17:09:20 +0200

    Migrations

--- Backend/Butcher_Shop/Migrations/20210507150859_fixes.Designer.cs
+++ Backend/Butcher_Shop/Migrations/20210507150859_fixes.Designer.cs
@@ -3,14 +3,16 @@ using Butcher_Shop.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 namespace Butcher_Shop.Migrations
 {
     [DbContext(typeof(DatabaseContext))]
-    partial class DatabaseContextModelSnapshot : ModelSnapshot
+    [Migration("20210507150859_fixes")]
+    partial class fixes
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ Backend/Butcher_Shop/Migrations/20210507150859_fixes.cs
@@ -0,0 +1,54 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Butcher_Shop.Migrations
+{
+    public partial class fixes : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Employees_ButcherStores_ButcherStoreId",
+                table: "Employees");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Employees_ButcherStoreId",
+                table: "Employees");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Password",
+                table: "Butchers",
+                type: "nvarchar(64)",
+                maxLength: 64,
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(16)",
+                oldMaxLength: 16);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "Password",
+                table: "Butchers",
+                type: "nvarchar(16)",
+                maxLength: 16,
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(64)",
+                oldMaxLength: 64);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Employees_ButcherStoreId",
+                table: "Employees",
+                column: "ButcherStoreId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Employees_ButcherStores_ButcherStoreId",
+                table: "Employees",
+                column: "ButcherStoreId",
+                principalTable: "ButcherStores",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+    }
+}
