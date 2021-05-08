a89ca9c89416407316485cc4f925471bdedeedd3
OmkoBass <omkob@hotmail.com>
2021-05-08 17:09:47 +0200

    Migrations

---
+++ Backend/Butcher_Shop/Migrations/20210508124649_ArticleNotUnique.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Butcher_Shop.Migrations
+{
+    public partial class ArticleNotUnique : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateIndex(
+                name: "IX_Employees_ButcherStoreId",
+                table: "Employees",
+                column: "ButcherStoreId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Employees_ButcherStores_ButcherStoreId",
+                table: "Employees",
+                column: "ButcherStoreId",
+                principalTable: "ButcherStores",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Employees_ButcherStores_ButcherStoreId",
+                table: "Employees");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Employees_ButcherStoreId",
+                table: "Employees");
+        }
+    }
+}
