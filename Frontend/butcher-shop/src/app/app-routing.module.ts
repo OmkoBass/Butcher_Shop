7755cb418a3bddb1b4e2c01027a570f37522d870
OmkoBass <omkob@hotmail.com>
2021-05-08 15:39:57 +0200

    Added new routes

--- Frontend/butcher-shop/src/app/app-routing.module.ts
+++ Frontend/butcher-shop/src/app/app-routing.module.ts
@@ -1,7 +1,9 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ButcherStoreComponent } from './butcher-store/butcher-store.component';
 import { DashboardComponent } from './dashboard/dashboard.component';
 import { AuthGuard } from './guards/auth.guard';
+import { HomeComponent } from './home/home.component';
 import { LoginComponent } from './login/login.component';
 import { RegisterComponent } from './register/register.component';
 
@@ -9,7 +11,17 @@ const routes: Routes = [
   {
     path: '',
     component: DashboardComponent,
-    canActivate: [AuthGuard]
+    canActivate: [AuthGuard],
+    children: [
+      {
+        path: '',
+        component: HomeComponent
+      },
+      {
+        path: 'butcherStore/:butcherStoreId',
+        component: ButcherStoreComponent
+      }
+    ]
   },
   {
     path: 'login',
