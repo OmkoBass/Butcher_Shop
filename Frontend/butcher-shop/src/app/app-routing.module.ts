6e51c665082ab89770f9fc728bd5741b61b78761
OmkoBass <omkob@hotmail.com>
2021-05-24 22:47:01 +0200

    Added new route

--- Frontend/butcher-shop/src/app/app-routing.module.ts
+++ Frontend/butcher-shop/src/app/app-routing.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { ButcherStoreComponent } from './butcher-store/butcher-store.component';
 import { DashboardComponent } from './dashboard/dashboard.component';
+import { EmployeesComponent } from './employees/employees.component';
 import { AuthGuard } from './guards/auth.guard';
 import { HomeComponent } from './home/home.component';
 import { LoginComponent } from './login/login.component';
@@ -16,6 +17,10 @@ const routes: Routes = [
       {
         path: '',
         component: HomeComponent
+      },
+      {
+        path: 'employees',
+        component: EmployeesComponent
       },
       {
         path: 'butcherStore/:butcherStoreId',
