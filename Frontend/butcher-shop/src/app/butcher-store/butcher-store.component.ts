39d3649481849c458e12dadcde3bbef1a5203bad
OmkoBass <omkob@hotmail.com>
2021-05-24 22:49:33 +0200

    Added back icon

--- Frontend/butcher-shop/src/app/butcher-store/butcher-store.component.ts
+++ Frontend/butcher-shop/src/app/butcher-store/butcher-store.component.ts
@@ -4,6 +4,7 @@ import { MatSnackBar } from "@angular/material/snack-bar";
 import { Router } from '@angular/router';
 import { MatDialog } from '@angular/material/dialog';
 import { EditEmployeeDialogComponent } from '../edit-employee-dialog/edit-employee-dialog.component';
+import { Location } from "@angular/common";
 
 @Component({
   selector: 'app-butcher-store',
@@ -14,7 +15,7 @@ export class ButcherStoreComponent implements OnInit {
   loading = true;
   butcherStore = null;
 
-  constructor(private butcherStoreService: ButcherStoreService, private router: Router, private snackBar: MatSnackBar, public dialog: MatDialog) { }
+  constructor(private butcherStoreService: ButcherStoreService, private router: Router, private snackBar: MatSnackBar, public dialog: MatDialog, private location: Location) { }
 
   ngOnInit(): void {
     // I'm gonna do it like this
@@ -24,12 +25,15 @@ export class ButcherStoreComponent implements OnInit {
     this.butcherStoreService.GetButcherStore(urlParam)
     .subscribe(res => {
       this.butcherStore = res;
-      console.log(res);
       this.loading = false;
     }, _ => {
       this.loading = false;
       this.snackBar.open('Something went wrong!', 'Okay!');
     });
+  }
+
+  handleGoBack() {
+    this.location.back();
   }
 
   handleOpenEditDialog(employee) {
