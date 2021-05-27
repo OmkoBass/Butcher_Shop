b3831a40b6892a59e571f4cdad72ee70301190d0
OmkoBass <omkob@hotmail.com>
2021-05-27 21:15:03 +0200

    Added a way to create new Butcher Stores and delete them

--- Frontend/butcher-shop/src/app/home/home.component.ts
+++ Frontend/butcher-shop/src/app/home/home.component.ts
@@ -1,6 +1,9 @@
 import { Component, OnInit } from '@angular/core';
+import { MatDialog } from '@angular/material/dialog';
 import { MatSnackBar } from '@angular/material/snack-bar';
+import { CreateButcherStoreDialogComponent } from '../create-butcher-store-dialog/create-butcher-store-dialog.component';
 import { ButcherService } from '../services/butcherService/butcher-service.service';
+import { ButcherStoreService } from '../services/butcherStoreService/butcher-store.service';
 
 @Component({
   selector: 'app-home',
@@ -11,7 +14,7 @@ export class HomeComponent implements OnInit {
   loading = true;
   butcher = null;
 
-  constructor(public butcherService: ButcherService, private snackBar: MatSnackBar) { }
+  constructor(public butcherService: ButcherService, private butcherStoreService: ButcherStoreService, private snackBar: MatSnackBar, public dialog: MatDialog) { }
 
   ngOnInit(): void {
     this.butcherService.GetButcher()
@@ -19,6 +22,29 @@ export class HomeComponent implements OnInit {
       this.butcher = res;
       this.loading = false;
     }, _ => {
+      this.snackBar.open('Something went wrong!', 'Okay!');
+    });
+  }
+  
+  handleOpenAddButcherStoreDialog() {
+    this.dialog.open(CreateButcherStoreDialogComponent, {
+      width: '60%',
+      minWidth: '320px'
+    })
+    .afterClosed()
+    .subscribe(res => {
+      if(res !== null) {
+        this.butcher.butcherStores.push(res.data);
+      }
+    });
+  }
+
+  handleDeleteStore(id) {
+    this.butcherStoreService.DeleteButcherStore(id)
+    .subscribe(_ => {
+      this.butcher.butcherStores = this.butcher.butcherStores.filter(store => store.id !== id); 
+      this.snackBar.open('Butcher Store deleted!', 'Okay!');
+    }, () => {
       this.snackBar.open('Something went wrong!', 'Okay!');
     });
   }
