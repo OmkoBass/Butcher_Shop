caf13f7444a83251c6c67924fdaa5c50ffbd3741
OmkoBass <omkob@hotmail.com>
2021-05-28 22:48:40 +0200

    Added migrations

---
+++ Backend/Butcher_Shop/Migrations/20210528194752_ArticleHasCustomerId.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Butcher_Shop.Migrations
+{
+    public partial class ArticleHasCustomerId : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
