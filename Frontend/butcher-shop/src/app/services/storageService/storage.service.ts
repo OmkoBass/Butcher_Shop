01aac395a5817c553d0e17435abd166b9ded783f
OmkoBass <omkob@hotmail.com>
2021-05-28 15:40:44 +0200

    Added storage services

---
+++ Frontend/butcher-shop/src/app/services/storageService/storage.service.ts
@@ -0,0 +1,29 @@
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class StorageService {
+  url = 'https://localhost:44323/api/Storage'
+
+  constructor(private http: HttpClient) { }
+
+  CreateStorage(storage) {
+    let headers = new HttpHeaders().set("Authorization", localStorage.getItem('beefyToken'));
+
+    return this.http.post<any>(this.url, storage, { headers });
+  }
+
+  DeleteStorage(id: string) {
+    let headers = new HttpHeaders().set("Authorization", localStorage.getItem('beefyToken'));
+
+    return this.http.delete<any>(`${this.url}/:id?Id=${id}`, { headers });
+  }
+
+  UpdateStorage(id: string, storage) {
+    let headers = new HttpHeaders().set("Authorization", localStorage.getItem('beefyToken'));
+
+    return this.http.put<any>(`${this.url}/:id?Id=${id}`, storage, { headers });
+  }
+}
