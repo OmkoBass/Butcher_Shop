5a685e23eb6457885b6d5b9f32d03b7c220da003
OmkoBass <omkob@hotmail.com>
2021-05-28 21:26:36 +0200

    Smarter search pipe implementation

--- Frontend/butcher-shop/src/app/pipes/searchPipe/search.pipe.ts
+++ Frontend/butcher-shop/src/app/pipes/searchPipe/search.pipe.ts
@@ -6,11 +6,7 @@ import { Pipe, PipeTransform } from '@angular/core';
 export class SearchPipe implements PipeTransform {
   transform(array: any, objectType: string, searchBy: string,): any {
     if(searchBy) {
-      if(objectType === 'employee') {
-        return array.filter(element => element.name.includes(searchBy));
-      } else if(objectType === 'storage') {
-        return array.filter(element => element.address.includes(searchBy));
-      }
+      return array.filter(element => element[objectType].includes(searchBy));
     }
     
     return array;
