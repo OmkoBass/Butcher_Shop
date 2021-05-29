26418487922bbd2c507c3f45bd8ad2d00e962005
OmkoBass <omkob@hotmail.com>
2021-05-29 12:39:10 +0200

    Added customer mapper

---
+++ Backend/Butcher_Shop/Profiles/CustomerProfile.cs
@@ -0,0 +1,19 @@
+﻿using AutoMapper;
+using Butcher_Shop.Dtos;
+using Butcher_Shop.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Butcher_Shop.Profiles
+{
+    public class CustomerProfile : Profile
+    {
+        public CustomerProfile()
+        {
+            CreateMap<Customer, AddCustomerDto>().ReverseMap();
+            CreateMap<Customer, CustomerDto>().ReverseMap();
+        }
+    }
+}
