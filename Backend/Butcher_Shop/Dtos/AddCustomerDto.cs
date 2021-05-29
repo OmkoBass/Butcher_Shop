c55d52c2ca5e663a8e43122f8c97267c79e0d4ec
OmkoBass <omkob@hotmail.com>
2021-05-29 12:39:01 +0200

    Added customer dtos

---
+++ Backend/Butcher_Shop/Dtos/AddCustomerDto.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Butcher_Shop.Dtos
+{
+    public class AddCustomerDto
+    {
+        [Required]
+        [MinLength(3)]
+        [MaxLength(16)]
+        public string Name { get; set; }
+
+        [Required]
+        [MinLength(3)]
+        [MaxLength(32)]
+        public string Lastname { get; set; }
+
+        [Required]
+        [MinLength(3)]
+        [MaxLength(64)]
+        public string Address { get; set; }
+
+        [Required]
+        public bool Sex { get; set; }
+    }
+}
---
+++ Backend/Butcher_Shop/Dtos/CustomerDto.cs
@@ -0,0 +1,23 @@
+﻿using AutoMapper;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Butcher_Shop.Dtos
+{
+    public class CustomerDto
+    {
+        public int Id { get; set; }
+
+        public string Name { get; set; }
+
+        public string Lastname { get; set; }
+
+        public string Address { get; set; }
+
+        public bool Sex { get; set; }
+
+        public ICollection<ArticleDto> BoughtArticles { get; set; }
+    }
+}
